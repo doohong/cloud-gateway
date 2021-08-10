4be822a7b158fc4f98674f88afcbd5650ad50d69
doohong <wnghd94@gmail.com>
2021-08-11 00:21:09 +0900

    게이트웨이 서버 config 서버 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     //jwt
     api("io.jsonwebtoken:jjwt:0.9.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/cloud/gateway/GatewayApplication.kt
+++ src/main/kotlin/com/cloud/gateway/GatewayApplication.kt
@@ -1,11 +1,20 @@
 package com.cloud.gateway
 
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class GatewayApplication
+class GatewayApplication {
+    @Bean
+    fun httpTraceRepository(): HttpTraceRepository {
+        return InMemoryHttpTraceRepository()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
+
